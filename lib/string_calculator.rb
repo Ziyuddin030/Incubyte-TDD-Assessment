3691156b2a34dd504d098fe028f5ffdf3e68518b
Ziyuddin030 <mohammadziyauddin030@gmail.com>
2025-06-26 21:05:40 +0530

    support custom delimiter defined with //<delimiter>\n

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,13 @@
 class StringCalculator
   def add(string_numbers)
     return 0 if string_numbers.empty?
-    numbers = string_numbers.split(/,|\n/).map(&:to_i)
+    delimiter = /,|\n/
+      if string_numbers.start_with?("//")
+        parts = string_numbers.split("\n", 2)
+        delimiter = Regexp.escape(parts[0][2]) # safely escape special chars
+        string_numbers = parts[1]
+      end
+    numbers = string_numbers.split(/#{delimiter}/).map(&:to_i)
     numbers.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,5 +25,10 @@ RSpec.describe StringCalculator do
       calc = StringCalculator.new
       expect(calc.add("1\n2,3")).to eq(6)
     end
+
+    it "supports custom single-character delimiter defined with // at the start" do
+      calc = StringCalculator.new
+      expect(calc.add("//;\n1;2")).to eq(3)
+    end
   end
 end
